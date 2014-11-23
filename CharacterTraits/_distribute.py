1c92b86928767dac9623b2117f71e3d4fc11089d
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-11-22 20:43:33 -0500

    Began Mac Compatibility
    Unfortunately, I need a Mac machine to install py2app :-1:

--- CharacterTraits/_distribute.py
+++ CharacterTraits/_distribute.py
@@ -12,31 +12,27 @@ script_file = 'RandomNPC.py'
 setup_args = dict()
     
 # Version: py2exe options
-import py2exe
-
-setup_args.update(dict(
-    console=[dict(script=script_file)],
-    options={
-        "py2exe": {
-            'bundle_files':1,
-            'compressed':True,
-            'ascii':True,
-            'dll_excludes':['w9xpopen.exe'],
-            'unbuffered':True
-            }}),
-                  zipfile = None)
+##import py2exe
+##setup_args.update(dict(
+##    console=[dict(script=script_file)],
+##    options={
+##        "py2exe": {
+##            'bundle_files':1,
+##            'compressed':True,
+##            'ascii':True,
+##            'dll_excludes':['w9xpopen.exe'],
+##            'unbuffered':True
+##            }}),
+##                  zipfile = None)
 
 # Version: py2app
-# try:
-#     import py2app
-#     setup_args.update(dict(
-#         app=[script_file],
-#         options=dict(py2app=dict(
-#             argv_emulation=True,
-#             iconfile='assets/app.icns',
-#         )),
-#     ))
-# except ImportError:
-#     pass
+import py2app
+setup_args.update(dict(
+    app=[script_file],
+    options=dict(py2app=dict(
+        argv_emulation=True,
+        iconfile='assets/app.icns',
+    )),
+))
 
 setup(**setup_args)
